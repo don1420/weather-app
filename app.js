a4fd1382d78a860a703c5d7ae029e5f335ba88bf
donyshchenko <donyshchenko@binaryanvil.com>
2023-07-05 13:54:56 +0300

    Created a callback function for application services and added a command line interface for user interaction

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-const request = require('postman-request')
+const readline = require('readline')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const mapboxApiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/zaporizhia.json?access_token=pk.eyJ1IjoiZnJlZW1hbjg2MTciLCJhIjoiY2xqb2NzcGNuMW02bzNxcGo0N3cxc2czdiJ9.eZg5CRqdxn1Y7ZUjQFSWug&limit=1'
-const weatherstackApiUrl = 'http://api.weatherstack.com/current?access_key=99bd0f3f0e3888763eb1bf3a937ce609&query=47.9205,35.0841"&units=m'
+const readLocation = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-request({url: mapboxApiUrl, json: true}, function (error, response, body) {
-  if (error) {
-    console.log(error)
-    console.log('Unable to connect to Mapbox API.')
-  } else if (body.features && body.features.length === 0) {
-    console.log('Unable to find location. Try another search.')
-  }
-  else {
-    const latitude = body.features[0].center[0]
-    const longitude = body.features[0].center[1]
-      console.log('latitude/longitude: ', latitude, '/', longitude)
-  }
-});
+const getWether = (location) => geocode(location, (error, data) => {
+    if (error) {
+        return console.log('Geocode error: ', error)
+    }
 
-request({url: weatherstackApiUrl, json: true}, function (error, response, body) {
-  if (error) {
-    console.log(error)
-    console.log('Unable to connect to Weatherstack API')
-  } else if (response.body.error) {
-    console.log('Unable to find location')
-  } else {
-    console.log(`It is currently ${response.body.current.temperature} degress out. It feels like ${response.body.current.feelslike} degress out.`)
-  }
-});
+    forecast(data.latitude, data.longitude, (forecastError, forecastData) => {
+        if (error) {
+            return onsole.log('Forecast error: ', forecastError)
+        }
+        console.log(data.location, 'latitude/longitude: ', data.latitude, '/', data.longitude)
+        console.log(`It is currently ${forecastData.temperature} degress out. It feels like ${forecastData.feelslike} degress out. ${forecastData.weather_descriptions}.`)
+    })
+})
+
+readLocation.question('Please enter your location: \n', (userInput) => {
+    if (userInput.trim() == '') {
+        readLocation.setPrompt('You have not entered your location. Please enter your location: \n')
+        readLocation.prompt()
+
+        readLocation.on('line', (userInput) => {
+            if (userInput.trim() == '') {
+                readLocation.setPrompt('You have not entered your location. Please enter your location: \n')
+                readLocation.prompt()
+            }
+            else {
+                getWether(userInput)
+                readLocation.close();
+            }
+        })
+    }
+
+    else {
+        getWether(userInput)
+        readLocation.close();
+    }
+})
